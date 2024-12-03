a8b58dcf21092a69d0d6b1e1998ee68f7cbe2de2
chloe <118663359+chloefrfr@users.noreply.github.com>
2024-12-02 20:42:26 -0500

    Cleanup, 0 ms for every endpoint!

--- install_packages.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Reload Backend Package Installer
-
-npm i
-npm install express
-npm install ws
-pause
--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Reload Backend
-
-:start
-node index.js
-goto start
