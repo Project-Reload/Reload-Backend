378584d7aa617db3ffdc64c03c8899949ccf0360
Burlone <108557545+burlone0@users.noreply.github.com>
2024-12-09 21:38:08 +0100

    Fixed sjcl module error, added removeall command
    Thanks iron web10 for removeall command

---
+++ DiscordBot/commands/Admin/removeall.js
@@ -0,0 +1,69 @@
+const { MessageEmbed } = require("discord.js");
+const path = require("path");
+const fs = require("fs");
+const Users = require('../../../model/user.js');
+const Profiles = require('../../../model/profiles.js');
+const destr = require("destr");
+const config = require('../../../Config/config.json')
+
+module.exports = {
+    commandInfo: {
+        name: "removeall",
+        description: "Allows you to remove all cosmetics from a user. Note: This will reset all your lockers to default",
+        options: [
+            {
+                name: "user",
+                description: "The user you want to give the cosmetic to",
+                required: true,
+                type: 6
+            }
+        ]
+    },
+    execute: async (interaction) => {
+
+        if (!config.moderators.includes(interaction.user.id)) {
+            return interaction.reply({ content: "You do not have moderator permissions.", ephemeral: true });
+        }
+
+        await interaction.deferReply({ ephemeral: true });
+
+        const selectedUser = interaction.options.getUser('user');
+        const selectedUserId = selectedUser?.id;
+        try {
+            const targetUser = await Users.findOne({ discordId: selectedUserId });
+            if (!targetUser) {
+                return interaction.editReply({ content: "That user does not own an account" });
+            }
+
+            const profile = await Profiles.findOne({ accountId: targetUser.accountId });
+            if (!profile) {
+                return interaction.editReply({ content: "That user does not have a profile" });
+            }
+
+            const allItems = destr(fs.readFileSync(path.join(__dirname, "../../../Config/DefaultProfiles/athena.json"), 'utf8'));
+            if (!allItems) {
+                return interaction.editReply({ content: "Failed to parse athena.json" });
+            }
+
+            Profiles.findOneAndUpdate({ accountId: targetUser.accountId }, { $set: { "profiles.athena.items": allItems.items } }, { new: true }, (err, doc) => {
+                if (err) {
+                    return interaction.editReply({ content: "There was an error updating the profile." });
+                }
+            });
+
+            const embed = new MessageEmbed()
+                .setTitle("Full Locker Removed")
+                .setDescription("Successfully removed all skins to the selected account")
+                .setColor("GREEN")
+                .setFooter({
+                    text: "Reload Backend",
+                    iconURL: "https://i.imgur.com/2RImwlb.png"
+                })
+                .setTimestamp();
+            await interaction.editReply({ embeds: [embed], ephemeral: true });
+        } catch (error) {
+            console.error("An error occurred:", error);
+            interaction.editReply({ content: "An error occurred while processing the request." });
+        }
+    }
+};
